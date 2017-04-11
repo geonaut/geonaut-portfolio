ffaaf2717542c57816b5443bfc99f4ec14fe70fb
Oli <oli@ddc.uk.net>
2017-04-11 14:14:37 +0100

    Adding some echoes to the build script

--- build/build.sh
+++ build/build.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 cd /vagrant/webapps/geonaut_venv/geonaut
+pwd
+echo "Git pull"
 git pull
+echo "activate venv"
 . /vagrant/webapps/geonaut_venv/bin/activate
 cd /vagrant/webapps/geonaut_venv/geonaut/geonaut
+pwd
+echo "running makemigrations"
 python /vagrant/webapps/geonaut_venv/geonaut/manage.py makemigrations
+echo "running migrate"
 python /vagrant/webapps/geonaut_venv/geonaut/manage.py migrate
+echo "running collectstatic"
 python /vagrant/webapps/geonaut_venv/geonaut/manage.py collectstatic --no-input
+echo "deactivate"
 deactivate
