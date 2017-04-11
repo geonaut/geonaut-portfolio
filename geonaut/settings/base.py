c9df3587dcca33f38b7e1632899ac7124bfdfc00
Oli <oli@ddc.uk.net>
2017-04-11 16:38:10 +0100

    Moving database details to sample local settings file

--- geonaut/settings/base.py
+++ geonaut/settings/base.py
@@ -90,18 +90,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'geonaut.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
