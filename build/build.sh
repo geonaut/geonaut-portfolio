b996d412169b004811358f086d73459cad40fd88
Oli <oli@ddc.uk.net>
2017-04-18 17:09:03 +0100

    Adding requirements.txt install, removing makemigrations

--- build/build.sh
+++ build/build.sh
@@ -6,10 +6,11 @@ echo "Git pull"
 git pull
 echo "activate venv"
 . /vagrant/webapps/geonaut_venv/bin/activate
+echo "installing requirements"
+cd /vagrant/webapps/geonaut_venv/geonaut/
+pip install -r requirements.txt
 cd /vagrant/webapps/geonaut_venv/geonaut/geonaut
 pwd
-echo "running makemigrations"
-python /vagrant/webapps/geonaut_venv/geonaut/manage.py makemigrations
 echo "running migrate"
 python /vagrant/webapps/geonaut_venv/geonaut/manage.py migrate
 echo "running collectstatic"
