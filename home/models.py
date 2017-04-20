619aec4bd618e0aec245f3262efdf4b1d7778f7c
Oli <oli@ddc.uk.net>
2017-04-20 11:42:15 +0100

    Adding feed image to project

--- home/models.py
+++ home/models.py
@@ -34,9 +34,17 @@ class ProjectsPage(Page):
 
 class ProjectsSubPage(Page):
     body = RichTextField(blank=True)
+    feed_image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
 
     content_panels = Page.content_panels + [
         FieldPanel('body', classname="full"),
+        ImageChooserPanel('feed_image'),
     ]
 
     @property
